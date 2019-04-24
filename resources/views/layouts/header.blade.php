d4c0713b79cd1b4d067a8ff001c24704c17cd365
hak055 <hak055-sahakyan@yandex.ru>
2019-04-24 10:05:50 +0300

    testVoyager

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -54,6 +54,7 @@
                                     Управление <span class="caret"></span>
                                   </a>
                                   <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                  	<a class="dropdown-item warning" href="/admin">Панель управления</a>
                                     <a class="dropdown-item warning" href="/tovar/create">Новый товар</a>
                                     <a class="dropdown-item" href="/">Главная страница</a>
                                     <a class="dropdown-item" href="/mark">Марка</a>
