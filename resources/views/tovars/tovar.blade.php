ad3285c2a106da0342f17b82a26e95ca22c66022
hak055 <hak055-sahakyan@yandex.ru>
2019-05-23 00:29:21 +0300

    add Builder for show all mark model

--- resources/views/tovars/tovar.blade.php
+++ resources/views/tovars/tovar.blade.php
@@ -10,7 +10,7 @@
                 <div class="list-group">
                     @if($marks->count())
                         @foreach($marks as $mark)
-                            <h3><a href="{{ route('mark.show', $mark) }}" class="list-group-item">{{$mark->name}}</a>
+                            <h3><a href="/?marka={{$mark->id}}" class="list-group-item">{{$mark->name}}</a>
                             </h3>
                         @endforeach
                     @else
@@ -20,21 +20,28 @@
             </div>
             <div class="col-md-8">
                 <div class="row" style="border: solid #1c8c58;">
-                    @foreach($tovars as $tovar)
-                        <div class="col-sm-4">
-                            <div class="product">
-                                <div class="product-img">
-                                    <a href=""><img src="#" alt=""></a>
+                    @if($tovars->count())
+                        @foreach($tovars as $tovar)
+                            <div class="col-sm-4">
+                                <div class="product">
+                                    <div class="product-img">
+                                        <a href=""><img src="#" alt=""></a>
+                                    </div>
+                                    <p class="product-title">
+                                        <a href="{{route('tovar.show', $tovar)}}">{{$tovar->PhoneModel->Mark->name.' '}}</a>
+                                    </p>
+                                    <p class="product-desc">{{$tovar->PhoneModel->name}}</p>
+                                    <!-- <p class="product-price">Price: €10.00</p> -->
+                                    <a href="{{route('tovar.show', $tovar)}}">{{$tovar->name}}</a>
                                 </div>
-                                <p class="product-title">
-                                    <a href="{{route('tovar.show', $tovar)}}">{{$tovar->PhoneModel->Mark->name.' '}}</a>
-                                </p>
-                                <p class="product-desc">{{$tovar->PhoneModel->name}}</p>
-                                <!-- <p class="product-price">Price: €10.00</p> -->
-                                <a href="{{route('tovar.show', $tovar)}}">{{$tovar->name}}</a>
                             </div>
-                        </div>
-                    @endforeach
+                        @endforeach
+                    @else
+                    <div class="spisok" style="text-align: center; padding: 50%;">
+                         <h1>Список пуст!!!</h1>  
+                    </div>
+                        
+                    @endif  
                 </div>
             </div>
         </div>
